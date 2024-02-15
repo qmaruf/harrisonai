30d8a3245d8e84ee576c61a5e1a5207167ebd3f7
Quazi Marufur Rahman <maruf@amagroup.io>
2024-02-15 14:03:19 +0000

    added segmentation head

--- src/network.py
+++ src/network.py
@@ -7,13 +7,23 @@ class PetNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.backbone = resnet18(weights=ResNet18_Weights.DEFAULT)
-        self.fc1 = nn.Linear(1000, 512)
-        self.fc2 = nn.Linear(512, 39)
-        self.sigmoid = nn.Sigmoid()
+        self.backbone.fc = nn.Identity()
+
+        self.seg_head = nn.Sequential(
+            nn.Unflatten(1, (16, 32, 1)),
+            nn.Upsample(size=(256, 256), mode="bilinear", align_corners=False),
+            nn.Conv2d(16, 1, kernel_size=3, padding=1, bias=False),
+        )
+
+        self.clf_head = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 39),
+            nn.Sigmoid(),
+        )
 
     def forward(self, x):
         x = self.backbone(x)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        x = self.sigmoid(x)
-        return x
+        clf = self.clf_head(x)
+        seg = self.seg_head(x)
+        return clf, seg
